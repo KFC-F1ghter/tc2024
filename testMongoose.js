a19b9217776947ba742954a21af3e9e74021f5d6
KFC-F1ghter <amoradfatima@gmail.com>
2025-11-26 22:14:25 +0300

    tc_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
+var schema = mongoose.Schema({ name: String })
 
-const Cat = mongoose.model('Cat', { name: String });
+
+schema.methods.meow = function(){
+   console.log(this.name + " сказал мяу")
+}
+
+const Cat = mongoose.model('Cat', schema);
 
 
 const kitty = new Cat({ name: 'Пушок' });
-kitty.save().then(() => console.log('Мяу'));
+kitty.save().then(() => kitty.meow());
