9c6e6bcacf98de830688510d1247b128dd1bdcef
KFC-F1ghter <amoradfatima@gmail.com>
2025-11-26 22:31:59 +0300

    tc_8_2

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/tc2024')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var indexCats = require('./routes/cats');
 
 var app = express();
 
@@ -23,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cats', indexCats);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
