5d6da7d183dc661b5edb3fcc9976511188f3f65c
KFC-F1ghter <amoradfatima@gmail.com>
2025-11-27 10:05:24 +0300

    tc_10_6

--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,67 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user').User;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', counter: req.session.counter });
+/* Middleware для передачи пользователя в шаблон */
+async function attachUser(req, res, next) {
+    res.locals.user = null;
+    if (req.session.user_id) {
+        try {
+            const user = await User.findById(req.session.user_id);
+            if (user) {
+                res.locals.user = user;
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    next();
+}
+
+/* GET home page */
+router.get('/', attachUser, function(req, res, next) {
+    res.render('index', { 
+        title: 'Три кота', 
+        counter: req.session.counter || 0 
+    });
 });
 
 /* GET login/registration page */
 router.get('/logreg', function(req, res, next) {
-    res.render('logreg', { title: 'Регистрация и вход' });
+    res.render('logreg', { title: 'Регистрация и вход', error: null });
 });
 
 /* POST login/registration page */
 router.post('/logreg', async function(req, res, next) {
-    var username = req.body.username;
-    var password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    console.log('Username:', username);
-    console.log('Password:', password);
+    if (!username || !password) {
+        return res.render('logreg', { title: 'Вход', error: 'Введите имя и пароль' });
+    }
 
-    var users = await User.find({ username: username });
-    console.log('Found users:', users);
+    try {
+        const users = await User.find({ username: username });
 
-    if (!users.length) {
-        // Пользователь НЕ найден → создаём нового
-        var user = new User({ username: username, password: password });
-        await user.save();
-        req.session.user_id = user._id;
-        res.redirect('/');
-    } else {
-        // Пользователь найден → проверяем пароль
-        var foundUser = users[0];
-        if (foundUser.checkPassword(password)) {
-            req.session.user_id = foundUser._id;
-            res.redirect('/');
+        if (!users.length) {
+            // Пользователь НЕ найден → создаём нового
+            const user = new User({ username: username, password: password });
+            await user.save();
+            req.session.user_id = user._id;
+            return res.redirect('/');
         } else {
-            // Пароль неверный → возвращаем на страницу логина с сообщением
-            res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
+            // Пользователь найден → проверяем пароль
+            const foundUser = users[0];
+            if (foundUser.checkPassword(password)) {
+                req.session.user_id = foundUser._id;
+                return res.redirect('/');
+            } else {
+                // Пароль неверный
+                return res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
+            }
         }
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
 });
 
