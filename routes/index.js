3eec2b09b88186c5740fad9618cf38db7b9cdc3b
KFC-F1ghter <amoradfatima@gmail.com>
2025-11-27 08:30:13 +0300

    tc_9_4

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
- req.session.greeting = "Hi!!!";
- res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express',  counter:req.session.counter });
 });
 
 
