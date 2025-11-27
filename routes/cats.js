6101949cb8112d0819374face90c21232ab20b62
KFC-F1ghter <amoradfatima@gmail.com>
2025-11-27 10:11:11 +0300

    tc_10_8

--- routes/cats.js
+++ routes/cats.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Cat = require('../models/cat').Cat;
-
-
+var checkAuth = require("../middlewares/checkAuth.js");
 
 /* Страница котов */
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick", checkAuth, async function(req, res, next) {
    var cats = await Cat.find({nick: req.params.nick});
    console.log(cats)
    if(!cats.length) return next(new Error("Нет такого котенка в мультфильме Три кота"))
